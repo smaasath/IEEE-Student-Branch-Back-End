bccfe890c6a5e0b37ebc3fde17b013bc3c673a6c
Aasath <aasadh2000@gmail.com>
2024-10-10 00:35:18 +0530

    docker

--- src/test/java/com/IEEEUWUSB/IEEEStudentBranchBackEnd/IeeeSBAppTests.java
+++ src/test/java/com/IEEEUWUSB/IEEEStudentBranchBackEnd/IeeeSBAppTests.java
@@ -2,8 +2,11 @@ package com.IEEEUWUSB.IEEEStudentBranchBackEnd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IeeeSBAppTests {
 
 	@Test
